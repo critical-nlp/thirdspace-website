7aa2deed988e71de423e1ae7534fadeb7901b17f
Raiyan007-gb <raiyan2025@gmail.com>
2026-07-11 22:18:16 +0600

    build(next.config): add production basePath config
    configure basePath to use the repository name when running in production mode for correct static hosting paths

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+const repoName = "thirdspace-website";
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: isProd ? `/${repoName}` : "",
   images: {
     unoptimized: true,
   },
